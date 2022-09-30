4f770f27aa56b91a1a52142bb1d876cfb7abed03
hanamura <takuya0hanamura@gmail.com>
2022-09-30 12:22:21 +0900

    Add: コンテンツ一覧取得アクションといいね機能アクションに、Auth0が連動するように実装

--- src/components/molecules/LikeButton.tsx
+++ src/components/molecules/LikeButton.tsx
@@ -1,37 +1,41 @@
 import "css/LikeButton.css";
-import { useState } from 'react';
-//import { useContext } from 'react';
-import axios from 'axios';
-import { useForm } from 'react-hook-form';
-import { REST_API_URL } from 'urls/index';
-//import { AuthGuardContext } from './../../providers/AuthGuard';
-import type { Like } from "types/like";
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import button from 'css/atoms/button.module.css';
-import { AnyMxRecord } from "dns";
+import { useAuth0 } from '@auth0/auth0-react';
+import { useContext } from 'react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { REST_API_URL } from 'urls/index';
+import { Auth0Context } from 'components/providers/AuthCheckprovider';
+import type { Like } from "types/like";
 
 export const LikeButton = (props: Like) => {
-  //const { accessToken } = useContext(AuthGuardContext);
+  const { accessToken, setAccessToken } = useContext(Auth0Context);
   const { id, changeLike } = props;
   const [LikedState, setLikedState] = useState(changeLike);
   const { register, handleSubmit } = useForm({});
-  const onSubmit = (data :any) => {
-    axios
-      .post(`${REST_API_URL}/user/likes`, data, {
-        headers: {
-          //Authorization: `Bearer ${accessToken}`,
-          'Content-Type': 'application/json',
-        },
-      })
-      .then((response) => {
-        setLikedState(true)
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+  const {isAuthenticated,getAccessTokenSilently } = useAuth0();
+  const UseonSubmit = (data :any) => {
+    const likeis = async () => {
+      const token = isAuthenticated ? getAccessTokenSilently() : null;
+      setAccessToken(token);
+      axios
+        .post(`${REST_API_URL}/user/content_video_likes`, data, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+          },
+        })
+        .then((response) => {
+          setLikedState(true)
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+      }
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(UseonSubmit)}>
       <input {...register('id', { value: id })} type='hidden' />
 
       <input type='submit' value={`いいね`} className={button.like} />
