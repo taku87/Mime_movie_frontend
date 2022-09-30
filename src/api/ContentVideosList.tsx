e22c6652e65aa9831009f6f238067e1f95f6469f
hanamura <takuya0hanamura@gmail.com>
2022-09-30 12:22:21 +0900

    Add: コンテンツ一覧取得アクションといいね機能アクションに、Auth0が連動するように実装

--- src/api/ContentVideosList.tsx
+++ src/api/ContentVideosList.tsx
@@ -1,11 +1,14 @@
-import axios from 'axios';
 import { useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import {
   QueryClient,
   QueryClientProvider,
   useQuery,
 } from 'react-query';
-
+import { useContext } from 'react';
+import { Auth0Context } from 'components/providers/AuthCheckprovider';
+import axios from 'axios';
+import { REST_API_URL } from 'urls/index';
 
 import { ContentVideoCard } from "components/organisms/ContentVideoCard";
 import type { ContentVideo } from "types/contentvideo";
@@ -13,17 +16,22 @@ import type { ContentVideo } from "types/contentvideo";
 //import CircularProgress from '@mui/material/CircularProgress';
 
 
-const ContentVideos = () => {
-  //const [isLoading, setIsLoading] = useState(false);
+const ContentVideos = ({ }) => {
+  const {isAuthenticated,getAccessTokenSilently } = useAuth0();
+  const { accessToken, setAccessToken } = useContext(Auth0Context);
   const [contentVideos, setContentVideos ] = useState<ContentVideo[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   // Queries
   //const query = useQuery<取得するデータ型, Error>('ユニークなクエリキー', データ取得関数);
   let { isLoading: queryLoading, data: content_videos } = useQuery(
     ['content_videos'],
     async () => {
+      const token = isAuthenticated ? await getAccessTokenSilently() : null;
+      setAccessToken(token);
       /** GETの処理 */
       const res = await axios
-      .get<ContentVideo[]>('http://localhost:3001/api/v1/user/content_videos');
+      .get<ContentVideo[]>(`${REST_API_URL}/user/content_videos`);
         setContentVideos(res.data);
       }
     );
