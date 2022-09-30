bb0373f9c960cf1756ae4b2c54ad6b3c0c66519e
hanamura <takuya0hanamura@gmail.com>
2022-09-30 12:27:25 +0900

    Improve:レイアウト編集

--- src/components/templates/ContentVideos.tsx
+++ src/components/templates/ContentVideos.tsx
@@ -1,14 +1,12 @@
-import SetContentVideo from "components/molecules/SetContentVideo";
+//import SetContentVideo from "components/molecules/SetContentVideo";
 import { SwingVideos } from "components/molecules/SwingVideos";
 import ContentVideosList from "api/ContentVideosList";
 
 export const  ContentVideos = () => {
   return(
 	<>
-	<h2>ContentsVideos</h2>;
-	<SetContentVideo />
+	<h2>ContentsVideos</h2>
   <SwingVideos />
-	<SetContentVideo />
 	<ContentVideosList />
 	</>
   );
