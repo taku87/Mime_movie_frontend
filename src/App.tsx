39650e14a704c37efda97034810894626a79c4fe
hanamura <takuya0hanamura@gmail.com>
2022-09-30 12:20:59 +0900

    Improve:ルーティングをapp.tsxから別ファイルに切り出す

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,30 @@
-// コンポーネント読み込み
-//import WrapMainContent from './components/WrapMainContent'
-
-// 共通スタイル読み込み
 import './css/App.css';
-
-// Route関連
-//ブラウザでアクセスしたURLのパスを元にルーティングして、
-//パスに紐づいているコンポーネントを表示する仕組み
-//import { Route, Switch } from 'react-router-dom';
-
-//不明なRouteは全てNotFound
-//const NotFound = () => {
-//  return(
-//    <h2>ページが見つかりません</h2>
-//  )
-//}
-import { Routes, Route } from 'react-router-dom';
-import Home from './components/templates/Home';
-import CompletedVideos from './components/templates/CompletedVideos';
-import CompletedVideo from './components/templates/CompletedVideo';
-import ContentsVideos from './components/templates/ContentVideos';
-import ContentsVideo from './components/templates/ContentVideo';
-import MyVideos from './components/templates/MyVideos';
-import Mypage from './components/templates/MyPage';
-import PrivacyPolicy from './components/templates/PrivacyPolicy';
-import Terms from './components/templates/Terms';
-import VideoPost from './components/templates/PostVideo';
-
-import NoMatch from './components/templates/NoMatch';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { AuthCheckProvider } from 'components/providers/AuthCheckprovider';
+import { BrowserRouter } from 'react-router-dom';
+import { Footer } from "components/organisms/Footer";
+import { Header } from "components/organisms/Header";
+import { Routers } from 'route/Routers';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div>
-        <h1>Hello React Router v6</h1>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<NoMatch />} />
-          <Route path="/completed_videos/" element={<CompletedVideos />}>
-            <Route path=":completedVideoId" element={<CompletedVideo />} />
-          </Route>
-          <Route path="/contents_videos/" element={<ContentsVideos />}>
-            <Route path=":contentsVideoId" element={<ContentsVideo />} />
-          </Route>
-          <Route path="/my_videos/" element={<MyVideos />} />
-          <Route path="/mypage/" element={<Mypage />} />
-          <Route path="/privacy_policy/" element={<PrivacyPolicy />} />
-          <Route path="/terms/" element={<Terms />} />
-          <Route path="/video_post/" element={<VideoPost />} />
-        </Routes>
-      </div>
+      <AuthCheckProvider>
+        <BrowserRouter>
+            <Header />
+            <Routers />
+            <Footer />
+        </BrowserRouter>
+      </AuthCheckProvider>
     </QueryClientProvider>
   );
-}
+};
 
-// React-Router情報取得
 export default App;
