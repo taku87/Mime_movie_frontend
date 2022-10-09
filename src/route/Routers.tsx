a93d082de0de13c22087ab3dc583000fc193146e
hana <100251756+taku87@users.noreply.github.com>
2022-10-09 12:52:02 +0900

    Fix:Homeページのリネーム

--- src/route/Routers.tsx
+++ src/route/Routers.tsx
@@ -1,7 +1,7 @@
 import { memo } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Home from 'components/templates/Home';
+import Top from 'components/templates/Top';
 import CompletedVideos from 'components/templates/CompletedVideos';
 import CompletedVideo from 'components/templates/CompletedVideo';
 import ContentsVideos from 'components/templates/ContentVideos';
@@ -17,7 +17,7 @@ import NoMatch from 'components/templates/NoMatch';
 export const Routers = memo(() => {
   return (
     <Routes>
-    <Route path="/" element={<Home />} />
+    <Route path="/" element={<Top />} />
     <Route path="*" element={<NoMatch />} />
     <Route path="/completed_videos/" element={<CompletedVideos />}>
       <Route path=":completedVideoId" element={<CompletedVideo />} />
