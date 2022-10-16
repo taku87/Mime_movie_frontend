3942c622e4490f4671aca4da0038c70acee067be
hana <100251756+taku87@users.noreply.github.com>
2022-10-17 05:49:42 +0900

    Add: react-spring導入（アニメーション） Flipcardのアニメーション作成

---
+++ src/components/molecules/FlipCardButton.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import { useSpring, a } from '@react-spring/web'
+
+import styles from 'src/css/molecules/styles.module.css'
+
+export const FlipCardButton = () => {
+  const [flipped, set] = useState(false)
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  })
+
+  return (
+    <Link to="/" className={styles.container} onClick={() =>  set(state => !state) }>
+      <a.div
+        className={`${styles.c} ${styles.back}`}
+        style={{ opacity: opacity.to(o => 1 - o), transform }}
+      />
+      <a.div
+        className={`${styles.c} ${styles.front}`}
+        style={{
+          opacity,
+          transform,
+          rotateX: '180deg',
+        }}
+      />
+    </Link>
+  )
+}
