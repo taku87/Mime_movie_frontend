f5110a138eb11f01a5b16d4673a1b13799cacfff
hana <100251756+taku87@users.noreply.github.com>
2022-10-29 12:16:34 +0900

    improve:共通するCSSデザインを統一する

--- src/components/pages/NoMatchs.tsx
+++ src/components/pages/NoMatchs.tsx
@@ -1,8 +1,11 @@
 import "src/css/globals/common.css";
+import "src/css/globals/text.css";
 
 export const NoMatch = () => {
   return (
-    <div>nomatch</div>
+    <div className="no-match" >
+      <h1 className="nom-match-text-first">nomatch</h1>
+    </div>
   )
 }
 
