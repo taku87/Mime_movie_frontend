203d401d488fae8c9e7979b8072b0a643d504261
hana <100251756+taku87@users.noreply.github.com>
2022-11-11 06:28:22 +0900

    Add:完成版動画が一覧表示できるように実装（rails側のコンテンツ動画テーブルにyoutube_urlというカラムを付加）

--- src/components/pages/CompletedVideosIndex.tsx
+++ src/components/pages/CompletedVideosIndex.tsx
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import {CompletedVideoShowCard} from "src/components/organisms/CompletedVideoShowCard";
+import { CompletedVideosList } from "src/components/organisms/CompletedVideosList";
 import "src/css/pages/CompletedVideosIndex.css";
 import "src/css/globals/common.css";
 
@@ -10,10 +10,9 @@ export const CompletedVideosIndex = () => {
         <div className="completed-videos-index-header">
             <h1 className="completed-videos-index-text-first" >Theater Room</h1>
         </div>
-          <CompletedVideoShowCard className="completed-video-set"  />
+          <CompletedVideosList className="completed-video-set"  />
       </div>
     </div>
-
   )
 }
 
