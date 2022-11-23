40056e0a39877632f56991e2736bec046fd223d2
hana <100251756+taku87@users.noreply.github.com>
2022-11-23 20:01:01 +0900

    Add:利用規約ページ追加

--- src/components/pages/Term.tsx
+++ src/components/pages/Term.tsx
@@ -1,8 +1,45 @@
-import "src/css/globals/common.css";
+import 'src/css/globals/common.css';
+import 'src/css/pages/Term.css';
+import 'src/css/globals/text.css';
+import { TermData } from 'src/components/atoms/TermData';
 
 export const Terms = () => {
   return (
-    <div className="term">Term</div>
+    <div className="term">
+      <h1 className="term-text-first" >利用規約</h1>
+      <h3 className="term-text-second">この利用規約（以下,「本規約」といいます。）は,MIME MOVIE（以下,「当社」といいます。）がこのウェブサイト上で提供するサービス（以下，「本サービス」といいます。）の利用条件を定めるものです。登録ユーザーの皆さま（以下，「ユーザー」といいます。）には，本規約に従って，本サービスをご利用いただきます。</h3>
+      <div className='card_textbox'>
+      <ul className='term-list'>
+        {TermData.map((value, key) => {
+          return(
+            <li key={key} >
+              <h1>{value.article}</h1>
+              <ol>
+              {value.paragraph.map((value, key) => {
+                return(
+                  <li key={key} >
+                    <p>{value.body}</p>
+                    { value.sub_paragraph ? (
+                      <ol>
+                      {value.sub_paragraph.map((value, key) => {
+                        return(
+                          <li key={key} >
+                            <p>{value.body}</p>
+                          </li>
+                        )
+                      })}
+                      </ol> ) : ( <></> )
+                    }
+                  </li>
+                )
+              })}
+              </ol>
+            </li>
+          )
+        })}
+      </ul>
+      </div>
+    </div>
   )
 }
 
