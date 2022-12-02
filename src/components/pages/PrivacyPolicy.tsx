0970c0ca0a4d6215d669e38adffdfa571f1b3206
hana <100251756+taku87@users.noreply.github.com>
2022-12-02 18:05:40 +0900

    Improve:利用規約、プライバシーポリシーの文章入力

--- src/components/pages/PrivacyPolicy.tsx
+++ src/components/pages/PrivacyPolicy.tsx
@@ -1,16 +1,16 @@
 import 'src/css/globals/common.css';
 import 'src/css/pages/PrivacyPolicy.css';
 import 'src/css/globals/text.css';
-import { TermData } from 'src/components/atoms/TermData';
+import { PrivacyPolicyData } from 'src/components/atoms/PrivacyPolicyData';
 
 export const PrivacyPolicy = () => {
   return (
     <div className="privacy-policy">
       <h1 className="privacy-policy-text-first" >プライバシーポリシー</h1>
-      <h3 className="privacy-policy-text-second">この利用規約（以下,「本規約」といいます。）は,MIME MOVIE（以下,「当社」といいます。）がこのウェブサイト上で提供するサービス（以下，「本サービス」といいます。）の利用条件を定めるものです。登録ユーザーの皆さま（以下，「ユーザー」といいます。）には，本規約に従って，本サービスをご利用いただきます。</h3>
+      <h3 className="privacy-policy-text-second">MIME MOVIE（以下，「当社」といいます。）は，本ウェブサイト上で提供するサービス（以下,「本サービス」といいます。）における，ユーザーの個人情報の取扱いについて，以下のとおりプライバシーポリシー（以下，「本ポリシー」といいます。）を定めます。</h3>
       <div className='card_textbox'>
       <ul className='privacy-policy-list'>
-        {TermData.map((value, key) => {
+        {PrivacyPolicyData.map((value, key) => {
           return(
             <li key={key} >
               <h1>{value.article}</h1>
